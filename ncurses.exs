0a99e8c8783736bae495ebaf20637796ac3c7e1e
Todd Martin <kj4ntv@gmail.com>
2025-07-26 22:02:36 -0700

    add: ncurses

---
+++ ncurses.exs
@@ -0,0 +1,37 @@
+defmodule Ncurses do
+  @behaviour Spellbook.Spell
+
+  @impl true
+  def name(), do: "ncurses"
+  @impl true
+  def description(), do: "Text-based UI Library"
+  @impl true
+  def version(), do: "6.5"
+  @impl true
+  def homepage(), do: "https://ftp.gnu.org/gnu/ncurses/ncurses-6.5.tar.gz"
+  @impl true
+  def license(), do: "ISC"
+  @impl true
+  def type(), do: :bin 
+  @impl true
+  def checksum(), do: ""
+  @impl true
+  def deps(), do: [
+    "autoconf",
+    "automake",
+    "libtool"
+  ]
+  @impl true
+  def source(), do: "https://ftp.gnu.org/gnu/ncurses/ncurses-6.5.tar.gz"
+  
+  @impl true
+  def install(args) do
+    with {_, 0} <- System.cmd("sh", ["configure", "--prefix=#{args.prefix}"], cd: args.cwd, into: IO.stream()),
+         {_, 0} <- System.cmd("make", [], cd: args.cwd, into: IO.stream()),
+         {_, 0} <- System.cmd("make", ["install"], cd: args.cwd, into: IO.stream()) do
+      :ok
+    else
+      {_, code} -> {:error, "Build step failed with code #{code}"}
+    end
+  end
+end
